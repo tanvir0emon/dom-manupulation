d85731184d33eb8ad327e3ae070f4b9c0eb1c4bd
tanvir0emon <tanvir.e61@gmail.com>
2020-11-08 21:12:27 +0600

    dom

---
+++ script.js
@@ -0,0 +1,6 @@
+function backgroundChange(){
+   const readMore = document.querySelectorAll(".read-more");
+   for (let i = 0; i <= readMore.length; i++){
+       readMore[i].getElementsByClassName.background = "dogerblue"
+   }
+}
